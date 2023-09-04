396630ec9e941b65eb3d6c9f5d0a98646a4d53f9
Mohit Gururani <mohitgururani12@gmail.com>
2023-09-04 22:07:53 +0530

    datatypes-basics

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+// Primitive "Call by Value"
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const myName = "Mohit"
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(typeof myName)
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman","naagraj","doga"]
